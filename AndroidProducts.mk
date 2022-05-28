05660aa3d991bee3e6237be40a8f8f6ab91891e4
no-leaks-inside <florianvictorcristea@gmail.com>
2022-05-29 00:44:15 +0300

    beyond1lte: Adapt For Evolution X snow

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond1lte.mk
+    $(LOCAL_DIR)/evolution_beyond1lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond1lte-eng \
-    lineage_beyond1lte-user \
-    lineage_beyond1lte-userdebug
+    evolution_beyond1lte-eng \
+    evolution_beyond1lte-user \
+    evolution_beyond1lte-userdebug
